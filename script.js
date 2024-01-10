e46f71adfedc7bda7f8100317d53c17bba66f8e9
Yuvraj Maurya <yuvraj95166@gmail.com>
2024-01-10 21:10:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+// Creating the student object
+const student = {
+  name: 'John'
+};
+
+// Adding getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Testing the getKeys() method on the student object
+console.log(student.getKeys()); // Output: ['name']
+
